d57abab9a5cbce05b260f431af75ed8ff47824fb
Thiago Ferrari <thiagofcjr@gmail.com>
2022-06-11 08:53:49 -0300

    review do projeto

--- frontEnd/src/ui/Home.js
+++ frontEnd/src/ui/Home.js
@@ -32,6 +32,30 @@ export default function BasicCard() {
 			<h1>
 				Escolha uma das opções ao lado esquerdo para começar.
 			</h1>
-		</div >
+
+			<h2 className={classes.titulo}>
+				Nosso About:
+			</h2>
+
+			<h4>
+				Este sistema foi desenvolvido para o curso de ADS, na disciplina de Tópicos Especiais em Programação.
+			</h4>
+			<h4>
+				O mesmo possui um CRUD entre as tabelas de Gerentes e Contas Correntes. Ademais foi trabalhado o conceito de inner join dentro do banco de dados. Na pasta /backend é possível ver todo script do criado para operar no MySQL.
+			</h4>
+
+			<h2 className={classes.titulo}>
+				Integrantes do Grupo:
+				<br />
+				LUIS GUSTAVO GIANVECCHIO SANTOS
+				<br />
+				MATHEUS ARAUJO FREITAS
+				<br />
+				OSMAR FONSECA NETO
+				<br />
+				THIAGO FERREIRA COSTA JUNIOR
+			</h2>
+
+		</div>
 	)
 }
